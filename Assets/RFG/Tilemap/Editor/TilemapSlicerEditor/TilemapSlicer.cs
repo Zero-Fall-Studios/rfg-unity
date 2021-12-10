946835bd3124a2117402e2636d0a962a7c1f3be2
Andrew Bliss <andrewgbliss@gmail.com>
2021-12-10 15:59:43 +0900

    updates

--- Assets/RFG/Tilemap/Editor/TilemapSlicerEditor/TilemapSlicer.cs
+++ Assets/RFG/Tilemap/Editor/TilemapSlicerEditor/TilemapSlicer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
@@ -42,7 +43,7 @@ namespace RFG
       rectsList = SortRects(rectsList, texture.width);
 
       string filenameNoExtension = Path.GetFileNameWithoutExtension(path);
-      var metas = new List<SpriteMetaData>();
+      var metaList = new List<SpriteMetaData>();
       int rectNum = 0;
 
       foreach (Rect rect in rectsList)
@@ -60,12 +61,30 @@ namespace RFG
           meta.name = filenameNoExtension + "_" + rectNum;
         }
         rectNum++;
-        metas.Add(meta);
+        metaList.Add(meta);
       }
 
-      importer.spritesheet = metas.ToArray();
+      var oldProperties = GetPhysicsShapeProperties(importer, metaList);
+
+      importer.spritesheet = metaList.ToArray();
+
+      EditorUtility.SetDirty(importer);
+
+      try
+      {
+        importer.SaveAndReimport();
+      }
+      catch (Exception e)
+      {
+        Debug.LogWarning("There was a problem with generating sprite file: " + e);
+      }
+
+      var newProperties = GetPhysicsShapeProperties(importer, metaList);
 
       AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+
+      AssetDatabase.Refresh();
+      AssetDatabase.SaveAssets();
     }
 
     static List<Rect> SortRects(List<Rect> rects, float textureWidth)
@@ -111,6 +130,71 @@ namespace RFG
         result = list;
       }
       return result;
+    }
+
+    public static Dictionary<string, SerializedProperty> GetPhysicsShapeProperties(TextureImporter importer, List<SpriteMetaData> metaList)
+    {
+      SerializedObject serializedImporter = new SerializedObject(importer);
+      var property = serializedImporter.FindProperty("m_SpriteSheet.m_Sprites");
+      var res = new Dictionary<string, SerializedProperty>();
+      var removed = new HashSet<int>();
+
+      for (int index = 0; index < property.arraySize; index++)
+      {
+        var name = importer.spritesheet[index].name;
+        if (res.ContainsKey(name))
+        {
+          continue;
+        }
+
+        var element = property.GetArrayElementAtIndex(index);
+        var physicsShape = element.FindPropertyRelative("m_PhysicsShape");
+
+        res.Add(name, physicsShape);
+        removed.Add(index);
+      }
+
+      return res;
+    }
+
+    public static void RecoverPhysicsShapeProperty(
+        Dictionary<string, SerializedProperty> newProperties,
+        Dictionary<string, SerializedProperty> oldProperties)
+    {
+
+      SerializedProperty property = null;
+      foreach (var item in newProperties)
+      {
+        if (!oldProperties.TryGetValue(item.Key, out var oldItem))
+        {
+          continue;
+        }
+
+        var newItem = item.Value;
+        if (oldItem.arraySize > 0)
+        {
+          newItem.arraySize = oldItem.arraySize;
+
+          for (int index = 0; index < newItem.arraySize; index++)
+          {
+            var newShape = newItem.GetArrayElementAtIndex(index);
+            var oldShape = oldItem.GetArrayElementAtIndex(index);
+            newShape.arraySize = oldShape.arraySize;
+
+            for (int pi = 0; pi < newShape.arraySize; pi++)
+            {
+              var newPt = newShape.GetArrayElementAtIndex(pi);
+              var oldPt = oldShape.GetArrayElementAtIndex(pi);
+              newPt.vector2Value = oldPt.vector2Value;
+            }
+          }
+
+          if (property == null)
+            property = newItem;
+        }
+      }
+
+      property?.serializedObject.ApplyModifiedPropertiesWithoutUndo();
     }
   }
 }
